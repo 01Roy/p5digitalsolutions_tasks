1e2b043344c7fde258276acd12e13947a2eaba04
Roy01 <subhamroy1180@gmail.com>
2024-02-10 13:09:57 +0530

    login implemented

--- router/signup.js
+++ router/signup.js
@@ -7,7 +7,12 @@ const router = express.Router()
 
 
 router.get('/', async (req, res) => {
-    res.send('someting')
+    const users = await Signupuser.find();
+    if (!users) {
+        res.send('no users exists')
+    }
+
+    res.status(200).send(users)
 })
 
 router.post('/', async (req, res) => {
@@ -19,21 +24,34 @@ router.post('/', async (req, res) => {
     })
 
     newUser = await newUser.save();
-    token = genToken(newUser)
-    console.log(token)
-
-
-
+    // token = genToken(newUser)
+    // console.log(token)
 
     if (!newUser) {
         return res.send("Unable to signup")
     }
 
-    res.send({ newUser, token })
-
-
+    res.send({ newUser })
 
 })
+
+router.post('/login', async (req, res) => {
+    const user = await Signupuser.findOne({ email: req.body.email })
+
+
+    if (!user) {
+        return res.send('User is not found')
+    }
+    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        token = genToken(user.id)
+        res.status(200).send({ user: user.name, token: token })
+    } else {
+        res.status(400).send('password is wrong')
+    }
+
+    res.send(user)
+})
+
 
 
 // router.post('/', userCreation)
